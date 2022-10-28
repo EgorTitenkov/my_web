fac94f22ac897fe477275fa1e7caf0dc5ba529bb
Егор Титенков <egortitenkov@MacBook-Air-Egor.local>
2022-10-28 16:15:28 +0300

    Adding cookie auth and error page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Web Site</title>
+</head>
+<body>
+    <?php require "components/header.php"?>
+    <div class="container mt-5"></div>
+        <h3 class="mb-5">Something goes wrong :(</h3>
+
+
+    </div>
+    <?php require "components/footer.php"?>
+</body>
+</html>
+
